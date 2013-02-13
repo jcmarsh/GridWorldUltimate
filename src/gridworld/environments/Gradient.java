6b327080c5e6f0d0216ac4d92299c15d295a919a
James <jcmarsh@jcmarsh-OptiPlex-990.(none)>
2013-02-13 15:23:58 -0500

    Updated readme file and .gitignore. Will begin on Gradient.java soon.

---
+++ src/gridworld/environments/Gradient.java
@@ -0,0 +1,66 @@
+package gridworld.environments;
+
+import javax.swing.JPanel;
+
+import simulator.Physics;
+import simulator.PlannableObject;
+
+
+/**
+ * Gradient.java
+ * An environment with a 2D gradient to be used in the boundary estimation problem.
+ * 
+ * February 2013
+ * @author James Marshall <jcmarsh (at) gwmail.gwu.edu>
+ */
+public class Gradient implements simulator.Environment {
+
+	@Override
+	public JPanel getSettingsPanel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public JPanel getViewPanel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void doDraw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setTime(double time) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public PlannableObject initNewPlannable(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(Physics physics) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isDone() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
